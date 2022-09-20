50cc04d3a250e18eb92fdcbbf6854664ad20268f
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2022-09-20 16:36:17 -0300

    feat: DatabaseError and Interceptor

---
+++ src/common/filters/errors/interceptors/database.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { DatabaseError } from '../types/DatabaseError';
+import { handleDatabaseErrors } from '../utils/handle-database-errors.utils';
+import { isPrismaError } from '../utils/is-prisma-error.utils';
+
+@Injectable()
+export class DatabaseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (isPrismaError(error)) {
+          error = handleDatabaseErrors(error);
+        }
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
