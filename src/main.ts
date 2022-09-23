1c8098798131ee8a70f1cde64d76d94069c937c3
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2022-09-23 10:15:18 -0300

    feat: add initial configuration swagger main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConflictInterceptor } from './common/filters/errors/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/filters/errors/interceptors/database.interceptor';
@@ -9,6 +10,16 @@ import { UnauthorizedInterceptor } from './common/filters/errors/interceptors/un
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Blog')
+    .setDescription('The Simple Blog API Description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
